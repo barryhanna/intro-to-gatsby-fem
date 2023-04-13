51aa1a0ef6f746eef8316f8a5829d9ffab326e45
Barry Hanna <barryahanna@gmail.com>
2023-04-13 20:32:22 +0100

    Adding and querying site metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,31 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          description
+          image
+          siteUrl
+          title
+        }
+      }
+    }
+  `);
+
+  const meta = data?.site?.siteMetadata ?? {};
+
   return (
-    <main>
-      <h1>Hello Frontend Masters!</h1>
-      <Link to="/">About</Link>
-    </main>
+    <>
+      <header>
+        <Link to="/">{meta.title}</Link>
+      </header>
+      <main>
+        <h1>{meta.title}</h1>
+        <Link to="/">About</Link>
+      </main>
+    </>
   );
 }
